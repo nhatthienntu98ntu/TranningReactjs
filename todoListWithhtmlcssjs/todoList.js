4b68753e40e239279a50e3a789e74fdf13dce93b
Nguyen Nhat Thien <nhatthienntu98@gmail.com>
2020-11-20 18:20:36 +0700

    create react-hook-basic

--- todoListWithhtmlcssjs/todoList.js
+++ todoListWithhtmlcssjs/todoList.js
@@ -1,28 +1,28 @@
 var arrItems = [];
 let deleteItemIndex = -1;
 
-let addButtom = document.querySelector(".todo-action-add");
-let deleteButtom = document.querySelector(".todo-action-delete");
+let addButtom = document.querySelector('.todo-action-add');
+let deleteButtom = document.querySelector('.todo-action-delete');
 let submitButtom = document.querySelector("input[type='submit']");
-let inputTitle = document.querySelector("#input-title");
-let inputDescription = document.querySelector("#input-description");
+let inputTitle = document.querySelector('#input-title');
+let inputDescription = document.querySelector('#input-description');
 
 function initData() {
   for (let i = 0; i < arrItems.length; i++) {
-    let item = document.createElement("div");
-    item.classList.add("todo-item");
-    item.setAttribute("onclick", `onviewDescription(${i})`);
+    let item = document.createElement('div');
+    item.classList.add('todo-item');
+    item.setAttribute('onclick', `onviewDescription(${i})`);
 
-    let title = document.createElement("p");
+    let title = document.createElement('p');
     title.innerHTML = arrItems[i].title;
     item.appendChild(title);
 
-    let todoList = document.querySelector(".todo-list");
+    let todoList = document.querySelector('.todo-list');
     todoList.appendChild(item);
   }
 }
 function removeData() {
-  let todoItems = document.querySelectorAll(".todo-item");
+  let todoItems = document.querySelectorAll('.todo-item');
   for (let i = 0; i < todoItems.length; i++) {
     todoItems[i].remove();
   }
@@ -37,46 +37,46 @@ function onviewDescription(index) {
       `.todo-item:nth-child(${Number(itemIndex) + 1})`
     );
     if (index === Number(itemIndex)) {
-      let todoContent = document.querySelector(".todo-description");
-      let todoAdd = document.querySelector(".todo-add");
-      let titleItem = document.querySelector("#title");
-      let descriptionItem = document.querySelector("#description");
+      let todoContent = document.querySelector('.todo-description');
+      let todoAdd = document.querySelector('.todo-add');
+      let titleItem = document.querySelector('#title');
+      let descriptionItem = document.querySelector('#description');
 
       titleItem.innerHTML = arrItems[index].title;
       descriptionItem.innerHTML = arrItems[index].description;
 
-      if (todoContent.classList.contains("display-none")) {
-        if (!todoAdd.classList.contains("display-none")) {
-          todoAdd.classList.add("display-none");
+      if (todoContent.classList.contains('display-none')) {
+        if (!todoAdd.classList.contains('display-none')) {
+          todoAdd.classList.add('display-none');
         }
-        todoContent.classList.remove("display-none");
+        todoContent.classList.remove('display-none');
       }
-      if (!itemClick.classList.contains("item-action")) {
-        itemClick.classList.add("item-action");
+      if (!itemClick.classList.contains('item-action')) {
+        itemClick.classList.add('item-action');
       }
-    } else if (itemClick.classList.contains("item-action")) {
-      itemClick.classList.remove("item-action");
+    } else if (itemClick.classList.contains('item-action')) {
+      itemClick.classList.remove('item-action');
     }
   }
 }
 
-addButtom.addEventListener("click", () => {
-  let todoContent = document.querySelector(".todo-description");
-  let todoAdd = document.querySelector(".todo-add");
-  if (todoAdd.classList.contains("display-none")) {
-    if (!todoContent.classList.contains("display-none")) {
-      todoContent.classList.add("display-none");
+addButtom.addEventListener('click', () => {
+  let todoContent = document.querySelector('.todo-description');
+  let todoAdd = document.querySelector('.todo-add');
+  if (todoAdd.classList.contains('display-none')) {
+    if (!todoContent.classList.contains('display-none')) {
+      todoContent.classList.add('display-none');
     }
-    todoAdd.classList.remove("display-none");
+    todoAdd.classList.remove('display-none');
   }
 });
 
-deleteButtom.addEventListener("click", () => {
+deleteButtom.addEventListener('click', () => {
   for (let i = 0; i < arrItems.length; i++) {
     if (i === deleteItemIndex) {
       arrItems.splice(deleteItemIndex, 1);
-      document.querySelector("#title").innerHTML = "";
-      document.querySelector("#description").innerHTML = "";
+      document.querySelector('#title').innerHTML = '';
+      document.querySelector('#description').innerHTML = '';
       removeData();
       initData();
     }
@@ -91,8 +91,8 @@ function addItem() {
     });
     removeData();
     initData();
-    inputTitle.value = "";
-    inputDescription.value = "";
+    inputTitle.value = '';
+    inputDescription.value = '';
   } else {
     // alert("Input not required");
     if (!inputTitle.value) {
@@ -103,16 +103,17 @@ function addItem() {
   }
 }
 
-submitButtom.addEventListener("click", () => addItem());
+submitButtom.addEventListener('click', () => addItem());
 
-inputTitle.addEventListener("keydown", (event) => {
+inputTitle.addEventListener('keydown', (event) => {
   if (event.keyCode === 13) {
     addItem();
   }
 });
 
-inputDescription.addEventListener("keydown", (event) => {
+inputDescription.addEventListener('keydown', (event) => {
   if (event.keyCode === 13) {
     addItem();
   }
 });
+const a = 'asdasd';
